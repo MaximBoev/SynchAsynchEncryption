32af47a61f9d41246f6a19bb1eed16b15fe2f550
maxim boev <maxim-boev@mail.ru>
2024-04-04 13:57:16 +0300

    OpenSSL

--- SynchAsynchEncryption/SynchAsynchEncryption.cpp
+++ SynchAsynchEncryption/SynchAsynchEncryption.cpp
@@ -1,9 +1,10 @@
 #include "SynchAsynchEncryption.h"
-
+#include <openssl/aes.h>
 SynchAsynchEncryption::SynchAsynchEncryption(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    
 }
 
 SynchAsynchEncryption::~SynchAsynchEncryption()
--- SynchAsynchEncryption/main.cpp
+++ SynchAsynchEncryption/main.cpp
@@ -7,4 +7,5 @@ int main(int argc, char *argv[])
     SynchAsynchEncryption w;
     w.show();
     return a.exec();
+    
 }
